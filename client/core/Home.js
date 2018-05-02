64e0e5b7dafadab6ed141a685ff894cf79512db7
Shama Hoque <shama.hoque@gmail.com>
2018-05-02 17:57:21 +0600

    GameDetail to show game lists in Home

--- client/core/Home.js
+++ client/core/Home.js
@@ -2,39 +2,43 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
 import Card, {CardContent, CardMedia} from 'material-ui/Card'
-import Typography from 'material-ui/Typography'
-import seashellImg from './../assets/images/seashell.jpg'
+import {list} from '../game/api-game.js'
+import GameDetail from '../game/GameDetail'
 
 const styles = theme => ({
-  card: {
-    maxWidth: 600,
-    margin: 'auto',
-    marginTop: theme.spacing.unit * 5
-  },
-  title: {
-    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
-    color: theme.palette.text.secondary
-  },
-  media: {
-    minHeight: 330
+  root: {
+    flexGrow: 1,
+    margin: '10px 24px',
   }
 })
 
 class Home extends Component {
+  state={
+    games: []
+  }
+  componentDidMount = () => {
+    list().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        this.setState({games: data})
+      }
+    })
+  }
+  updateGames = (game) => {
+    const updatedGames = this.state.games
+    const index = updatedGames.indexOf(game)
+    updatedGames.splice(index, 1)
+    this.setState({games: updatedGames})
+  }
   render() {
     const {classes} = this.props
     return (
-        <Card className={classes.card}>
-          <Typography type="headline" component="h2" className={classes.title}>
-            Home Page
-          </Typography>
-          <CardMedia className={classes.media} image={seashellImg} title="Unicorn Shells"/>
-          <CardContent>
-            <Typography type="body1" component="p">
-              Welcome to the MERN VR Game home page.
-            </Typography>
-          </CardContent>
-        </Card>
+      <div className={classes.root}>
+        {this.state.games.map((game, i) => {
+          return <GameDetail key={i} game={game} updateGames={this.updateGames}/>
+        })}
+      </div>
     )
   }
 }
